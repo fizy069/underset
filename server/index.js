89c7d34dee330ba154bbd95eb10d2b2dac8cfd43
reckt32 <divyanshsing5nov2003@gmail.com>
2025-01-26 08:04:38 +0530

    hell yes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin');
+const recruiterRoutes = require('./routes/recruiter');
+const studentRoutes = require('./routes/student');
+
+
 
 
 // Middleware
@@ -30,6 +41,11 @@ app.use((err, req, res, next) => {
       error: process.env.NODE_ENV === 'development' ? err.message : {}
     });
   });
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes); 
+app.use('/api/recruiter', recruiterRoutes); 
+app.use('/api/student', studentRoutes); 
+
   
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
