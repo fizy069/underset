91d0435b277e5992302d48f6924984c1cc1478d2
reckt32 <divyanshsing5nov2003@gmail.com>
2025-01-26 23:07:59 +0530

    admin

--- server/models/job.js
+++ server/models/job.js
@@ -3,6 +3,7 @@ const jobSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String },
     eligibilityCriteria: { type: String },
+    college: { type: mongoose.Schema.Types.ObjectId, ref: 'College' },
     company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true }, // Reference to the company
     recruiter: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // The recruiter who posted the job
     applicants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'StudentProfile' }], // List of student applicants
